a6ad04ef627778009522d0abe889784d4b7a3dfd
Tony G. Bolaño <tony@tonygb.com>
2023-03-06 22:35:23 +0100

    feat: cómo se llama a la API desde JS

--- balance/static/js/balance.js
+++ balance/static/js/balance.js
@@ -1,6 +1,13 @@
+const peticion = new XMLHttpRequest();
+console.log("Empiezo a ejecutar JS");
+
 function cargarMovimientos() {
   console.log('Has llamado a la función cargarMovimientos()');
-}
+
+  peticion.open('GET', 'http://localhost:5000/api/v1/movimientos', false);
+  peticion.send();
+  console.log(peticion.responseText);
+};
 
 window.onload = function () {
   console.log('Función anónima al finalizar la carga de la ventana');
