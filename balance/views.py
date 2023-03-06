51f047b82d605d64cd24025807463365540b6817
Tony G. Bolaño <tony@tonygb.com>
2023-03-06 20:18:39 +0100

    feat: definición de API REST

--- balance/views.py
+++ balance/views.py
@@ -1,6 +1,25 @@
 from . import app
 
 
+"""
+Verbos y formato de endpoints
+
+GET /movimientos ----------> LISTAR movimientos
+POST /movimientos ---------> CREAR un movimiento nuevo
+
+GET /movimientos/1 --------> LEER el movimiento con ID=1
+POST /movimientos/1 -------> ACTUALIZAR el movimiento con ID=1 (sobreescribe todo el objeto)
+PUT /movimientos/1 --------> ACTUALIZAR el movimiento con ID=1 (sobreescribe parcialmente)
+DELETE /movimientos/1 -----> ELIMINAR el movimiento con ID=1
+
+IMPORTANTE!!!
+Versionar los endpoint (son un contrato)
+/api/v1/...
+
+
+"""
+
+
 @app.route('/')
 def inicio():
     return (f'La ruta del archivo de datos es: {app.config["RUTA"]}<br>'
